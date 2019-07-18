ce9e9709b42c7618ef61d814b3308f4ab820cc85
jmuzsik <jerrymuzsik@icloud.com>
2019-07-18 01:39:49 -0400

    small stylistic changes

---
+++ src/components/Tags/Tags.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import kebabCase from 'lodash/kebabCase';
+import styles from './Tags.module.scss';
+
+export default function Tags({ tags }) {
+  return (
+    <ul className={styles['tags']}>
+      {tags.map(tag => (
+        <li key={tag.fieldValue}>
+          <Link to={`/tag/${kebabCase(tag.fieldValue)}/`}>
+            {tag.fieldValue} ({tag.totalCount})
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- src/templates/tags-list-template.js
+++ src/templates/tags-list-template.js
@@ -1,35 +1,25 @@
 // @flow
 import React from 'react';
-import { Link } from 'gatsby';
-import kebabCase from 'lodash/kebabCase';
 import Layout from '../components/Layout';
 import Sidebar from '../components/Sidebar';
 import Page from '../components/Page';
 import Waves from '../components/Waves/Waves';
+import Tags from '../components/Tags/Tags';
 import { useSiteMetadata, useTagsList } from '../hooks';
 
 const TagsListTemplate = () => {
   const { title, subtitle } = useSiteMetadata();
   const tags = useTagsList();
-  console.log('tags list template');
-  return (
-    <React.Fragment>
 
-    <Layout title={`Tags - ${title}`} description={subtitle}>
-      <Sidebar />
-      <Page title="Tags">
-        <ul>
-          {tags.map((tag) => (
-            <li key={tag.fieldValue}>
-              <Link to={`/tag/${kebabCase(tag.fieldValue)}/`}>
-                {tag.fieldValue} ({tag.totalCount})
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </Page>
-    </Layout>
-    <Waves />
+  return (
+    <React.Fragment className="tags">
+      <Layout title={`Tags - ${title}`} description={subtitle}>
+        <Sidebar />
+        <Page title="Tags">
+          <Tags tags={tags} />
+        </Page>
+      </Layout>
+      <Waves />
     </React.Fragment>
   );
 };
